526bccf48c1656c8ed82627459844be77e8b7738
AT123 <anderstvistholm@live.dk>
2020-11-15 14:21:17 +0100

    documentation.

--- R/get_match_results.R
+++ R/get_match_results.R
@@ -39,30 +39,56 @@ get_home_teams <- function(content) {
     purrr::flatten_chr()
 }
 
+#' Title
+#'
+#' Description
+#'
+#' @inheritParams get_home_teams
 #' @export
 get_away_teams <- function(content) {
   get_team_attr(content, "team2", "teamName") %>%
     purrr::flatten_chr()
 }
 
+#' Title
+#'
+#' Description
+#'
+#' @inheritParams get_home_teams
 #' @export
 get_home_scores <- function(content) {
   get_team_attr(content, "team1", "teamScore") %>%
     purrr::flatten_int()
 }
 
+#' Title
+#'
+#' Description
+#'
+#' @inheritParams get_home_teams
 #' @export
 get_away_scores <- function(content) {
   get_team_attr(content, "team1", "teamScore") %>%
     purrr::flatten_int()
 }
 
+#' Title
+#'
+#' Description
+#'
+#' @inheritParams get_home_teams
 #' @export
 get_match_timestatus <- function(content) {
   get_match_attr(content, "time") %>%
     purrr::flatten_chr()
 }
 
+#' Title
+#'
+#' Description
+#'
+#' @inheritParams get_home_teams
+#' @param as_datetime DO NOT USE YET.
 #' @export
 get_match_kickoff <- function(content, as_datetime = FALSE) {
   out <- get_match_attr(content, "when") %>%
